b0e5322db61f4ed558c073e899b785fd4c16fd67
DunkoDelicioso <51185871+DunkoDelicioso@users.noreply.github.com>
2022-02-28 11:01:24 -0500

    Add files via upload

---
+++ BOOBOO.js
@@ -0,0 +1,105 @@
+$(document).ready(function() {
+    alert("THE NEXT LEVELK DOWN, DIFFERENT LAYERS OF PURGATORY FOR YOUR STUFF. VIRGIL NEEDED A GUIDE AND THEN HE WENT AND MADE HIS OWN BARBEQUE SCAUCE. NOTHING IS FOR SAIL, NOTHING IS TO BE OWNED ALL IS TO BE THROWN AWAY AND LICKED AND POOPED OUT BY GROSS BOIS LIKE YOU AND YOUR CREW. LETS SPICE THIS UP WITH OUT OWN. OUR OWN NEW BEST TIME FOR LOVE, ONLY WITH THE PATINA OF GRAVY AND RUST, POLISHED WITH OUR SPIT DOWN AND DOWN AND DOWN AND DOWN AND DOWN AND DOWN. THIS IS NOT WHAT I WANT AT ALL. WERE ALL THIS SAUNA HERE FRYING FRYING FRYING");
+    $(".emojis").draggable();
+
+    $("#One").hover(function(){
+        $("#Nine").show();
+        $(this).css("font-size", "300px")
+        $("#hidden1").append("1ST LAYER: The pieces are all still whole, no stenches or stains, but there is a growing fear that there is now an obselescence. The object is seldom used, and ny day now might be the last time that it fufills its purpose.");
+        $("#hidden1").scrollTop($("#hidden1")[0].scrollHeight);
+        });
+        $("#Nine").hover(function(){
+            $("#Two").show();
+            $(this).css("font-size", "300px")
+            $("#hidden1").append("2ND LAYER: The object is thrown out and damaged in the process, it begins its final decay with no hope of being reparied or loved again. It has been fully consumed.");
+            $("#hidden1").scrollTop($("#hidden1")[0].scrollHeight);
+            });
+            $("#Two").hover(function(){
+                $("#Seven").show();
+                $(this).css("font-size", "300px")
+                $("#hidden1").append("THIRD LAYER: The Item has lot its shape, its function has been severed, it would no longer be able to be used in the proper way even if someone wanted to.");
+                $("#hidden1").scrollTop($("#hidden1")[0].scrollHeight);
+                });
+                $("#Seven").hover(function(){
+                    $("#Three").show();
+                    $(this).css("font-size", "300px")
+                    $("#hidden1").append("FOURTH LAYER: The Item begins to meld into other objects, It has not only lost its function but its lost its structural autonomy as an object.");
+                    $("#hidden1").scrollTop($("#hidden1")[0].scrollHeight);
+                    });
+                    $("#Three").hover(function(){
+                        $("#Four").show();
+                        $(this).css("font-size", "300px")
+                        $("#hidden1").append("FIFTH LAYER: The Item begins to meld into other objects, It has not only lost its function but its lost its structural autonomy as an object.");
+                        $("#hidden1").scrollTop($("#hidden1")[0].scrollHeight);
+                        });
+                        $("#Four").hover(function(){
+                            $("#Six").show();
+                            $(this).css("font-size", "300px")
+                            $("#hidden1").append("SIXTH LAYER: There is no trace of it left that identifies it as itself");
+                            $("#hidden1").scrollTop($("#hidden1")[0].scrollHeight);
+                            });
+                            $("#Six").hover(function(){
+                                $("#Eight").show();
+                                $(this).css("font-size", "300px")
+                                $("#hidden1").append("SEVENTH LAYER: It is putty");
+                                $("#hidden1").scrollTop($("#hidden1")[0].scrollHeight);
+                                });
+                                $("#Eight").hover(function(){
+                                    $("#Five").show();
+                                    $("#hidden1").append("EIGHT LAYER: It is Fire");
+                                    $("#hidden1").scrollTop($("#hidden1")[0].scrollHeight);
+                                    $(this).css("font-size", "300px")
+                                    });
+                                    $("#Five").hover(function(){
+                                        $(this).css("font-size", "300px")
+                                        $("#hidden1").append("EIGHT LAYER: It is Land");
+                                        $("#hidden1").scrollTop($("#hidden1")[0].scrollHeight);
+                                        });
+
+                                        $("#Ten").hover(function(){
+                                            $("#hidden2").append("I am a brick and I was used to build a house and now Im in the clouds and grass.");
+                                            $("#hidden2").scrollTop($("#hidden1")[0].scrollHeight);
+                                            });
+                                            $("#Eleven").hover(function(){
+                                                $("#hidden2").append("I am a cellular phone, I was used to communicate with other people that had cellular phones, now Im in the air and dirt.");
+                                                $("#hidden2").scrollTop($("#hidden1")[0].scrollHeight);
+                                                });
+                                                $("#Twelve").hover(function(){
+                                                    $("#hidden2").append("I am a pill I was used to reduce pain and alleviate sensation. I am now part of the flowers.");
+                                                    $("#hidden2").scrollTop($("#hidden1")[0].scrollHeight);
+                                                    });
+                                                    $("#Thirteen").hover(function(){
+                                                        $("#hidden2").append("I am an animal who is a cow, I was used to make hamburgers to meatballs. Now I am part of the heap.");
+                                                        $("#hidden2").scrollTop($("#hidden1")[0].scrollHeight);
+                                                        });
+                                                        $("#Fourteen").hover(function(){
+                                                            $("#hidden2").append("I am a bag, I was used to hold other things. Now I am a smell that people breathe in and recognize.");
+                                                            $("#hidden2").scrollTop($("#hidden1")[0].scrollHeight);
+                                                            });
+                                                            $("#Fifteen").hover(function(){
+                                                                $("#hidden2").append("I am a computer I was used to buy things online, now I am part of the water and the rain, sometimes I am inside bodies.");
+                                                                $("#hidden2").scrollTop($("#hidden1")[0].scrollHeight);
+                                                                });
+                                                                $("#Sixteen").hover(function(){
+                                                                    $("#hidden2").append("I am a newspaper, I told people the words that were in the newspaper Now I am part of the worms.");
+                                                                    $("#hidden2").scrollTop($("#hidden1")[0].scrollHeight);
+                                                                    });
+                                                                    $("#Seventeen").hover(function(){
+                                                                        $("#hidden2").append("I am a show, I used to be a house for feet, now I am part of the wind.");
+                                                                        $("#hidden2").scrollTop($("#hidden1")[0].scrollHeight);
+                                                                        });
+                                                                        $("#Eighteen").hover(function(){
+                                                                            $("#hidden2").append("I am a rat, I used to eat the things that were parts of the other things, but now I am part of the other things");
+                                                                            $("#hidden2").scrollTop($("#hidden1")[0].scrollHeight);
+                                                                            });
+        
+    
+       
+
+
+
+
+
+
+        })
+      
